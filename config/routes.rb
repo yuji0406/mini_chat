c543c979fc2794b3e8b7670e5aabf6b8e7d9e513
yuji0406 <ky19930305@gamil.com>
2022-01-27 05:54:26 +0000

    updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :users, only: %i[show edit update]
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get 'rooms/index'
   get 'messages/index'
   get 'messages/new'
-  get 'users/show'
-  get 'users/edit'
-  get 'homes/top'
+  root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
