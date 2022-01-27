23c745cad418564ee6b309cfa1f0375fcc11f6e7
yuji0406 <ky19930305@gamil.com>
2022-01-27 08:22:36 +0000

    finished user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,36 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "登録が完了しました！"
+      redirect_to @user
+    else
+      flash.now[:danger] = "登録に失敗しました・・・"
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, 
+                                   :campany_name, 
+                                   :email,
+                                   :is_admin,
+                                   :introduction,
+                                   :password, 
+                                   :password_confirmation
+                                   )
+    end
 end
