0cf8d5a481af731649b7ef4a9c5835f9d91b8fcc
yuji0406 <ky19930305@gamil.com>
2022-01-27 06:48:58 +0000

    updated_user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ApplicationRecord
   before_save { email.downcase! }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
-                   format: { with: VALID_EMAIL_REGEX },
-                   uniqueness: {case_sensitive: false}
+                  format: { with: VALID_EMAIL_REGEX },
+                  uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: { minimum: 7 }
   validates :name, presence: true
   validates :campany_name, presence: true
   validates :introduction, length: { maximum: 200 }
   attribute :is_admin, :boolean, default: false
-  has_many :user_rooms, dependent: destroy
-  has_many :messages, dependent: destroy
-  
+  has_many :user_rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :chats, dependent: :destroy
 end
