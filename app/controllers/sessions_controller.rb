6fc44f15a7745ff6a3ce9d5254071659b8d9b48a
yuji0406 <ky19930305@gamil.com>
2022-01-27 10:55:27 +0000

    finished secure login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
     def create
     user = User.find_by(email: params[:session][:email].downcase)
       if user && user.authenticate(params[:session][:password])
-        log_in(user)
+        log_in user
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
         redirect_to user
       else
         
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
     end
     
     def destroy
-      log_out
+      log_out if logged_in?
       flash[:success] = "ログアウトしました！"
       redirect_to root_path
     end
