3ae306850f545ee6475cea4d09c229c004b43c3b
yuji0406 <ky19930305@gamil.com>
2022-01-27 06:01:44 +0000

    updated message_model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ApplicationRecord
+  validates :message, presence: true,
+                      length: { maximum: 200 }
+  attribute :is_admin_message, :boolean, default: false
+  belongs_to :user
 end
